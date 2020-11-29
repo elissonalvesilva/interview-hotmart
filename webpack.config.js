e077f87c232854880bb75a79b5ddff3d63929d22
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-11-29 14:17:00 -0400

    feat: init project

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack')
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: 'html-loader'
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HTMLWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    })
+  ],
+  devtool: false,
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    hot: true
+  }
+};
