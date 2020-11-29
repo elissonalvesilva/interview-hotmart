f83bde6a56b1523d31b03baec495894f1017c192
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-11-29 14:57:55 -0400

    feat: integrate react router dom into project

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import QuickOps from '../pages/QuickOps';
 
 function App() {
-  return <h1>Hello Word</h1>;
+  return (
+    <Router>
+      <>
+        <Switch>
+          <Route exact path="/" component={QuickOps} />
+        </Switch>
+        {/* <AppSidebar />
+        <Container>
+          <AppPageHeader />
+            <PrivateRoute exact path="/" component={Home} />
+            <PrivateRoute exact path="/store-config" component={StoreConfig} />
+            <PrivateRoute exact path="/store-config/help" component={Help} />
+          </Switch>
+        </Container>
+        <AppLoading /> */}
+      </>
+    </Router>
+  );
 }
 
 export default App;
