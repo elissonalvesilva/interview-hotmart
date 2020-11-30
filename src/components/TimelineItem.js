9fcb719aa1726faefbdfd7820deb5ac9f60bd00f
Elisson Alves Silva <elisson@192.168.1.9>
2020-11-30 12:38:52 -0400

    feat: create evaluation and expense item in timeline

--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -6,8 +6,23 @@ import Strings from '../utils/strings';
 function TimelineItem({ item }) {
   const EvaluationItem = () => {
     return (
-      <div className="item__evaluation">
-        <div className="" />
+      <div className="row item__evaluation">
+        <div className="col-3">
+          <div className="row">
+            <div className="col-12 item__icon">
+              <span className="icon__evaluation">*</span>
+            </div>
+            <span className="col-12 item__date">
+              {Strings.formartDate(item.cardDate)}
+            </span>
+          </div>
+        </div>
+        <div className="col-9">
+          <label className="item__label">Tipo</label>
+          <p className="item__description">
+            Despesas enviadas para aprovação {item.author.name}
+          </p>
+        </div>
       </div>
     );
   };
@@ -60,8 +75,21 @@ function TimelineItem({ item }) {
 
   const ExpenseItem = () => {
     return (
-      <div className="item__expense">
-        <div className="" />
+      <div className="row item__expense">
+        <div className="col-3">
+          <div className="row">
+            <div className="col-12 item__icon">
+              <span className="icon__expense">*</span>
+            </div>
+            <span className="col-12 item__date">
+              {Strings.formartDate(item.cardDate)}
+            </span>
+          </div>
+        </div>
+        <div className="col-9">
+          <label className="item__label">Tipo</label>
+          <p className="item__description">Despesas enviadas para aprovação</p>
+        </div>
       </div>
     );
   };
