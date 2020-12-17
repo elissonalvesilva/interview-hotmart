3ba1d06b4c3be84ddf8e2f9aa3fc91b7ed07d39d
Elisson Alves Silva <elisson@192.168.1.10>
2020-12-16 21:08:48 -0400

    feat: create expense component (starting component)

---
+++ src/components/Expense.js
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+
+function Expense() {
+  const [shownExpeseForm, setShowExpenseForm] = useState(false);
+
+  const showForm = () => {
+    setShowExpenseForm(true);
+  };
+
+  return (
+    <div className="expense-app">
+      <div className="expense__actions">
+        <button type="button">Inserir notas em lote</button>
+        <button type="button" onClick={showForm}>
+          Adicionar despensas
+        </button>
+      </div>
+      {shownExpeseForm && (
+        <div className="expense__container">
+          <div className="row expense__content">
+            <h2>Nova despesa</h2>
+            <div className="col-6 expense__upload">
+              <input type="file" />
+            </div>
+            <div className="col-6 expense__data">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="selectType" className="form-label">
+                    Tipo*
+                  </label>
+                  <select
+                    className="form-select form-control"
+                    id="selectType"
+                    aria-label="Default select example"
+                  >
+                    <option defaultValue>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="expenseTitle" className="form-label">
+                    Tipo de despesa*
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="expenseTitle"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="basic-url" className="form-label">
+                    Data do comprovante*
+                  </label>
+                  <div className="input-group ">
+                    <span className="input-group-text" id="basic-addon1">
+                      <i className="far fa-calendar" />
+                    </span>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Username"
+                      aria-label="Username"
+                      aria-describedby="basic-addon1"
+                    />
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="expenseTitle" className="form-label">
+                    Valor da nota / cupom*
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="expenseTitle"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="expenseTitle" className="form-label">
+                    Valor a ser considerado*
+                  </label>
+                  <input
+                    className="form-control"
+                    type="text"
+                    id="expenseTitle"
+                  />
+                </div>
+                <div className="expense-form__actions">
+                  <button type="button">Cancelar</button>
+                  <button type="submit">Salvar</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Expense;
--- src/pages/QuickOps.js
+++ src/pages/QuickOps.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Header from '../components/Header';
 
+import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
+import Expense from '../components/Expense';
 import Timeline from '../components/Timeline';
 
 function QuickOps() {
@@ -9,6 +10,7 @@ function QuickOps() {
     <div className="quickops d-flex justify-content-between">
       <section className="quickops-section flex-fill">
         <Header />
+        <Expense />
         <Timeline />
       </section>
       <Sidebar />
