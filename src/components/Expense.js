7e737dc182685f2e2a09a949f8e2baff7ad4722c
Elisson Alves Silva <elisson@192.168.1.9>
2020-12-21 21:59:26 -0400

    feat: styling expense component

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import Button from './layout/Button';
+import Card from './layout/Card';
+
 import Conditional from './layout/Conditional';
 
 function Expense() {
-  const [shownExpeseForm, setShowExpenseForm] = useState(false);
+  // trocar antes de commitar
+  const [shownExpeseForm, setShowExpenseForm] = useState(true);
 
   const showForm = () => {
     setShowExpenseForm(true);
@@ -11,90 +15,128 @@ function Expense() {
   return (
     <div className="expense-app">
       <div className="expense__actions">
-        <button type="button">Inserir notas em lote</button>
-        <button type="button" onClick={showForm}>
+        <button type="button" className="add-receipt-button" onClick={showForm}>
+          <i className="fas fa-receipt mr-2" />
           Adicionar despensas
         </button>
       </div>
       <Conditional condition={shownExpeseForm}>
-        <div className="expense__container">
-          <div className="row expense__content">
-            <h2>Nova despesa</h2>
-            <div className="col-6 expense__upload">
-              <input type="file" />
-            </div>
-            <div className="col-6 expense__data">
-              <form>
-                <div className="mb-3">
-                  <label htmlFor="selectType" className="form-label">
-                    Tipo*
-                  </label>
-                  <select
-                    className="form-select form-control"
-                    id="selectType"
-                    aria-label="Default select example"
-                  >
-                    <option defaultValue>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                  </select>
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="expenseTitle" className="form-label">
-                    Tipo de despesa*
-                  </label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    id="expenseTitle"
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="basic-url" className="form-label">
-                    Data do comprovante*
-                  </label>
-                  <div className="input-group ">
-                    <span className="input-group-text" id="basic-addon1">
-                      <i className="far fa-calendar" />
-                    </span>
+        <Card className="expense__container" title="Nova despesa">
+          <div className="expense__content">
+            <form>
+              <div className="row form__data">
+                <div className="col-6 expense__upload">
+                  <span className="upload__label-title">
+                    Recibo, cupom ou nota fiscal*
+                  </span>
+                  <i className="fas fa-question-circle upload__icon-info" />
+                  <div className="upload__form-control">
+                    <label htmlFor="formFile" className="upload__label">
+                      Selecione um arquivo do seu computador
+                    </label>
                     <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Username"
-                      aria-label="Username"
-                      aria-describedby="basic-addon1"
+                      type="file"
+                      className="form-control upload__input-file"
+                      id="formFile"
                     />
                   </div>
+                  <small className="upload__caption">
+                    A imagem deve estar no formato JPG ou PNG.
+                  </small>
                 </div>
-                <div className="mb-3">
-                  <label htmlFor="expenseTitle" className="form-label">
-                    Valor da nota / cupom*
-                  </label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    id="expenseTitle"
-                  />
+                <div className="col-5 expense__data">
+                  <div className="row m-0">
+                    <div className="col-6 p-0">
+                      <label htmlFor="selectType" className="form-label">
+                        Tipo*
+                      </label>
+                      <select
+                        className="form-select form-control"
+                        id="selectType"
+                        aria-label="Default select example"
+                      >
+                        <option defaultValue>Selecione</option>
+                        <option value="hotel-fee">Hotel Fee</option>
+                        <option value="food">Food</option>
+                        <option value="transport">Transport</option>
+                      </select>
+                    </div>
+                    <div className="col-6 pr-0 mb-3">
+                      <label htmlFor="selectType" className="form-label">
+                        Moeda*
+                      </label>
+                      <select
+                        className="form-select form-control"
+                        id="selectType"
+                        aria-label="select currency"
+                      >
+                        <option defaultValue>Selecione</option>
+                        <option value="BRL">BRL</option>
+                        <option value="USD">USD</option>
+                        <option value="MXN">MXN</option>
+                      </select>
+                    </div>
+                    <div className="col-12 p-0 mb-3">
+                      <label htmlFor="expenseTitle" className="form-label">
+                        Titulo da despesa*
+                      </label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        id="expenseTitle"
+                      />
+                    </div>
+                    <div className="col-12 p-0 mb-3">
+                      <label htmlFor="basic-url" className="form-label">
+                        Data do comprovante*
+                      </label>
+                      <div className="input-group ">
+                        <span className="input-group-text" id="basic-addon1">
+                          <i className="far fa-calendar" />
+                        </span>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Username"
+                          aria-label="Username"
+                          aria-describedby="basic-addon1"
+                        />
+                      </div>
+                    </div>
+                    <div className="col-6 pl-0 mb-3">
+                      <label htmlFor="expenseTitle" className="form-label">
+                        Valor da nota / cupom*
+                      </label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        id="expenseTitle"
+                      />
+                    </div>
+                    <div className="col-6 pr-0 mb-3">
+                      <label htmlFor="expenseTitle" className="form-label">
+                        Valor a ser considerado*
+                      </label>
+                      <input
+                        className="form-control"
+                        type="text"
+                        id="expenseTitle"
+                      />
+                    </div>
+                  </div>
                 </div>
-                <div className="mb-3">
-                  <label htmlFor="expenseTitle" className="form-label">
-                    Valor a ser considerado*
-                  </label>
-                  <input
-                    className="form-control"
-                    type="text"
-                    id="expenseTitle"
-                  />
-                </div>
-                <div className="expense-form__actions">
-                  <button type="button">Cancelar</button>
-                  <button type="submit">Salvar</button>
-                </div>
-              </form>
-            </div>
+              </div>
+              <div className="expense-form__actions">
+                <Button type="button" className="cancel">
+                  Cancelar
+                </Button>
+                <Button type="submit" className="submit">
+                  Submit
+                </Button>
+              </div>
+            </form>
           </div>
-        </div>
+        </Card>
       </Conditional>
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,53 @@
 const path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.html$/,
-        use: [{
-          loader: 'html-loader'
-        }]
-      }
-    ]
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new webpack.ProgressPlugin(),
     new HTMLWebpackPlugin({
       template: './public/index.html',
-      filename: 'index.html'
-    })
+      filename: 'index.html',
+    }),
   ],
   devtool: false,
   devServer: {
     contentBase: path.join(__dirname, 'public'),
     compress: true,
-    hot: true
-  }
+    hot: true,
+  },
 };
