6609d15fc7094a02ef67dffe602d9f1daa345989
Elisson Alves Silva <elisson@192.168.1.9>
2020-12-21 21:37:08 -0400

    refactor: change style card style

--- src/components/layout/Card.js
+++ src/components/layout/Card.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import classnames from 'classnames';
+import Conditional from './Conditional';
 
 function Card({ title = '', children = {}, className = '' }) {
-  const renderTitle = () => {
-    return title ? <h1 className="card-title">{title}</h1> : null;
-  };
+  const renderTitle = () => (
+    <Conditional condition={title}>
+      <div className="card-title">
+        <h1>{title}</h1>
+      </div>
+    </Conditional>
+  );
 
   return (
     <div className={classnames('card card-app', className)}>
